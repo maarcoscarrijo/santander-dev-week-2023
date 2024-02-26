7aa65e5182720f4f507908b033c1882e6a143b4a
marcoscarrijo <carrijo.x@hotmail.com>
2024-02-26 16:43:35 -0300

    Implementacao da camada de dominio

--- src/main/java/me/dio/domain/model/Account.java
+++ src/main/java/me/dio/domain/model/Account.java
@@ -16,10 +16,10 @@ public class Account {
 
     private String agency;
 
-    @Column(nullable = false, scale = 13, precision = 2)
+    @Column(nullable = false, precision = 13, scale = 2)
     private BigDecimal balance;
 
-    @Column(name = "addtional_limit", nullable = false, scale = 13, precision = 2)
+    @Column(name = "addtional_limit", nullable = false, precision = 13, scale = 2)
     private BigDecimal limit;
 
 
